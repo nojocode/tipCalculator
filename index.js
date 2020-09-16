d299081ddf928ce3181ac307847f7c2621e5bb2a
Nojo code <njartworks@gmail.com>
2020-09-15 22:16:19 -0400

    Attempt to create new calc

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // edit the TipCalculator function so it has two
 // parameters, mealCost and tipPercent
 // instead of the hard coded 20% (0.2)
-function tipCalculator(mealCost) {
-  return mealCost * 0.2;
+function tipCalculator() {
+
+    //Change the code for the meal amount
 }
-let tipInput = document.querySelector("#tipAmount");
+let mealAmt = document.getElementById("mealAmt").value;
 // get the user input out of the 1st text box
 // give a variable name to the 1st text box
-let userInput = document.querySelector("#mealAmount");
+let tiplevel = document.getElementById("#tiplevel").value;
 // pay attention to changes in userInput
-userInput.addEventListener("change", function () {
-  console.log(userInput.value);
-  // run the tipCalculator function
-  // using the number the user entered for the meal
-  let newTip = tipCalculator(userInput.value);
-  // show that tip in the 2nd text box
-  tipInput.value = newTip;
+userInput.addEventListener("radio checked", function() {
+    console.log(userInput.value);
+    // run the tipCalculator function
+    // using the number the user entered for the meal
+    let newTip = tipCalculator(userInput.value);
+    // show that tip in the 2nd text box
+    tipInput.value = newTip;
 });
