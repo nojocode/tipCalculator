36e6ad2a0ddfbfe450640c711c1b42bf62fa7c20
Nojo code <njartworks@gmail.com>
2020-09-19 10:10:57 -0400

    Update to the code on calculator

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // edit the TipCalculator function so it has two
 // parameters, mealCost and tipPercent
 // instead of the hard coded 20% (0.2)
-function tipCalculator() {
+function tipCalculator(totalAmount) {}
 
-    //Change the code for the meal amount
-}
-let mealAmt = document.getElementById("mealAmt").value;
+let mealInput = document.querySelector("#mealAmount");
 // get the user input out of the 1st text box
 // give a variable name to the 1st text box
-let tiplevel = document.getElementById("#tiplevel").value;
-// pay attention to changes in userInput
-userInput.addEventListener("radio checked", function() {
-    console.log(userInput.value);
-    // run the tipCalculator function
-    // using the number the user entered for the meal
-    let newTip = tipCalculator(userInput.value);
-    // show that tip in the 2nd text box
-    tipInput.value = newTip;
-});
+let tipPercent;
+if (document.getElementById("tipPercent15").checked) tipPercent = 15;
+if (document.getElementById("tipPercent18").checked) tipPercent = 18;
+if (document.getElementBYId("tipPercent20").checked) tipPercent = 20;
+
+let total = mealAmount * tipPercent;
+
+let Total = parseFloat(mealAmount) + parseFloat(totalAmount);
+
+document.getElementById("totalAmount").innerHTMl = total;
+}
